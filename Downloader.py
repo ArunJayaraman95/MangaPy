c5ba4d83eb51abdb21932a258824a3ae56367e1b
Arun Jayaraman <riparun.aj@gmail.com>
2024-03-14 23:50:12 -0400

    Fixed double logging bug

--- Downloader.py
+++ Downloader.py
@@ -27,23 +27,6 @@ ch.setFormatter(formatter)
 logger.addHandler(ch)
 
 
-logger = logging.getLogger('MANGAPY')
-logger.setLevel(logging.DEBUG)
-
-# create console handler and set level to debug
-ch = logging.StreamHandler()
-ch.setLevel(logging.DEBUG)
-
-# create formatter
-formatter = logging.Formatter('%(name)s[%(levelname)s]: %(message)s')
-
-# add formatter to ch
-ch.setFormatter(formatter)
-
-# add ch to logger
-logger.addHandler(ch)
-
-
 class MainWindow(QDialog):
     def __init__(self):
         super(MainWindow, self).__init__()
